aac77f5d017af461a0800af6ed0b7dc94793218b
Andrei Lupu <andreilupu>
2013-06-10 15:16:20 +0300

    Done

--- create-trial.php
+++ create-trial.php
@@ -1,6 +1,6 @@
 <?php
 
-$trial = new wpGrade_Trial();
+global $trial;
 
 $username = $_GET['username'];
 $email = $_GET['user_email'];
@@ -16,22 +16,25 @@ if( !isset($_GET['theme']) || empty( $_GET['theme']) )  {
     $blog_id = $theme[0];
     $theme_name = $theme[1];
 
-    $user_id = $trial->create_user_for_trial($username, $email);
+    $user = $trial->create_user_for_trial($username, $email);
 
-    if ( is_wp_error( $user_id ) ) {
+    if ( is_wp_error( $user['id'] ) ) {
 
-        echo $user_id->get_error_message();
+        echo $user['id']->get_error_message();
 
     } else {
-        echo '<pre style="background-color: #ebebeb">';
-        $trial->create_new_trial($username, $user_id, $email, $blog_id, $theme_name );
+        echo '<pre style="background-color: #ebebeb"><h2>Process log </h2>';
+
+        echo '<p>'.$user['msg'].'</p>';
+
+        $trial->create_new_trial($username, $user['id'], $email, $blog_id, $theme_name );
         echo '</pre>';
     }
 
     if ( !empty($blog_id) ) {
         echo '<p>Thank you for your submision! <br /> Check your email or <a href="/">Return</a>.</p>';
 
-        $user = get_user_by( 'id', $user_id );
+        $user = get_user_by( 'id',  $user['id'] );
         echo 'Hey ' . $username . ' your account is created with the password "admin"';
         echo '<p>Check your new website here: <a href="http://77.81.241.142/'.$theme_name.'-'.$username.'">http://77.81.241.142/'.$theme_name.'-'.$username.'</a></p>';
     }
--- functions.php
+++ functions.php
@@ -2,56 +2,68 @@
 
 class wpGrade_Trial {
 
-    protected $domain;
+    protected $role;
+    protected $log_file;
 
     public function __construct(){
 
-        $this->domain = 'localhost';
+        $this->role = "Trial";
+        $this->log_file = get_template_directory() . '/trial-logs';
+        add_action('wp_ajax_clear_old_trials', array( $this, 'clear_old_trials' ), 1 );
+        add_action('wp_ajax_nopriv_clear_old_trials', array( $this, 'clear_old_trials' ), 1 );
         return;
     }
 
+    protected function log_to_file( $str, $user ){
+        $date = date('Y-m-d h-i-s');
+        $ip = $_SERVER["REMOTE_ADDR"];
+        $current = file_get_contents($this->log_file);
+        $current .= "***[ $date ]*** LOG: $str *** BY User: $user; IP : $ip; ***\r\n ";
+        file_put_contents( $this->log_file, $current );
+
+    }
+
     public function create_new_trial($username, $user_id, $admin_email, $template_id, $theme_name ){
-
-        $error = NULL;
-
+        $new_blog_id = 1;// for fallback
+        $error = array();
+        $success = array();
         //get the data from the array into strings
         $siteurl = $theme_name. '-' .$username;
-        $blogdescription = 'Trial '. $username;
+        $blogdescription = ucfirst( $theme_name). ' Trial for '. $username;
         $blogname = ucfirst($username);
         $dashedsiteurl = str_replace('.', '-', $siteurl);
         $domain = get_blog_details(1)->domain;
         $fulldomain = get_blog_details(1)->domain . "/" . $dashedsiteurl;
         $path = "/" . $dashedsiteurl;
-
         global $wpdb;
 
-        if(!$error){
+        if( empty( $error ) ){
             if($exist_id = $wpdb->get_var("SELECT blog_id FROM $wpdb->blogs WHERE path = '$path/'")) {
-                _e("<span class=\"error\">The URL $fulldomain already exist, we skipped it!</span>", 'wpgrade' );
-                $error = TRUE;
+                $this->log_to_file("The URL $fulldomain already exist, we skipped it!", $username );
+                $error['domain_exists'] = "This domain already exists.";
             } else {
                 // Start with adding the new blog to the blogs table
 
                 $new_blog_id = insert_blog( $domain, $path, '1');
 
                 if(is_integer($new_blog_id)) {
-                    _e("New site created with id: $new_blog_id<br/>", 'wpgrade' );
+                    $this->log_to_file("New site created with id: $new_blog_id", $username );
+                    $success['blog_created'] = "Your blog has been created";
+
                 } else {
-                    _e("<span class=\"error\">The URL $fulldomain already exist, we skipped it!</span>", 'wpgrade' );
-                    $error = TRUE;
+                    $this->log_to_file("The URL $fulldomain already exist, we skipped it!", $username );
+                    $error['domain_exists'] = "This domain already exists.";
                 }
             }
         }
 
-
         //Next duplicate all tables from the template
-        if(!$error){
+        if( empty( $error ) ){
 
             $template_like = $wpdb->prefix . $template_id . "_";
             $template_new = $wpdb->prefix . $new_blog_id . "_";
             $temp_like = str_replace('_', '\_', $template_like); //escape the _ for correct sql!!
             $template_tables = $wpdb->get_results( "SHOW TABLES LIKE '$temp_like%'", ARRAY_N );
-
 
             foreach ($template_tables as $old_table) {
                 $new_table = str_replace($template_like, $template_new, $old_table[0]);
@@ -61,54 +73,46 @@ class wpGrade_Trial {
                     // duplicate the old table structure
                     $result = $wpdb->query( "CREATE TABLE $new_table LIKE $old_table[0]" );
                     if($result === FALSE) {
-                        _e("<span class=\"error\">Failed to create $new_table.</span>", 'wpgrade' );
-                        $error = TRUE;
+                        $this->log_to_file("Failed to create $new_table.", $username );
+                        $error['creat_table'] = "Failed to create $new_table.";
                     } else {
-//                        _e("Table created: $new_table.<br>", 'wpgrade' );
                         // copy data from old_table to new_table
                         $result = $wpdb->query( "INSERT INTO $new_table SELECT * FROM $old_table[0]" );
                         if($result === FALSE) {
-                            _e("<span class=\"error\">Failed to copy data from $old_table[0] to $new_table.</span>", 'wpgrade' );
-                            $error = TRUE;
+                            $this->log_to_file("Failed to copy data from $old_table[0] to $new_table.", $username );
+                            $error['copy_table'] = "Failed to copy data from $old_table[0] to $new_table.";
                         } else {
 //                            _e("Copied data from $old_table[0] to $new_table.<br/>", 'wpgrade' );
-
-//                            if ( $new_table == $wpdb->prefix . $new_blog_id . "_posts" ) {
-//                                $wpdb->query( "UPDATE wp_posts WHERE post_type = `attachment` SET guid = REPLACE (guid, 'http://localhost/site', 'http://newsite')" );
-//                            }
-
                         }
                     }
                 } else {
-                    _e("<span class=\"error\">The table $new_table already existed.</span>", 'wpgrade' );
-                    $error = TRUE;
+                    $this->log_to_file("The table $new_table already existed.", 'wpgrade' );
+                    $error['table_exists'] = "The table $new_table already existed.";
                 }
             }
 
-            if ( !$error ) {
+            if ( empty($error) ) {
                 // update links for all attachments.
-
-
             }
         }
 
         // Then add user to the new blog
-        if(!$error) {
-            $role = "administrator";
+        if( empty($error) ) {
+            $role = $this->role;
             if ( add_user_to_blog( $new_blog_id, $user_id, $role ) ) {
-                _e( 'Added user '.$user_id.' as '.$role.' to site '.$new_blog_id.'.<br/>',  'wpgrade' );
+                $this->log_to_file("Added user ".$user_id.' as '.$role.' to site '.$new_blog_id.'.', $username );
 
                 // remove the new user from the main site ( only super-admin should be there)
                 remove_user_from_blog($user_id, 1);
 
             } else {
-                _e( 'Failed to add user '.$user_id.' as '.$role.' to site '.$new_blog_id.'.<br/>', 'wpgrade' );
-                $error = TRUE;
+                $this->log_to_file('Failed to add user '.$user_id.' as '.$role.' to site '.$new_blog_id.'.', $username );
+                $error['add_user_to_blog'] = 'Failed to add user '.$user_id.' as '.$role.' to site '.$new_blog_id.'.';
             }
         }
 
         // Add custom data to newly duplicated blog
-        if(!$error) {
+        if(empty($error)) {
             $full_url = "http://" . $fulldomain;
             if(!$blogname) { $blogname = $siteurl; }
             $fileupload_url = $full_url . "/files";
@@ -128,15 +132,15 @@ class wpGrade_Trial {
 
             // 'check' if it went ok - NOTE: is just a basic check could give an error anyway...
             if(get_blog_option($new_blog_id, 'blogdescription') != $blogdescription) {
-                //$error = TRUE;
-                _e("<span class=\"error\">Maybe we had an error updating the options table with the new data.</span>", 'wpgrade' );
+                $error['update_tables'] = 'Maybe we had an error updating the options table with the new data.';
+                $this->log_to_file("Maybe we had an error updating the options table with the new data.", $username );
             } else {
-                _e("Updated the options table with cloned data<br>", 'wpgrade' );
+                $this->log_to_file("Updated the options table with cloned data", $username );
             }
         }
 
         // add template_id to option table for later reference
-        if(!$error) {
+        if(empty($error)) {
             $savearray = array ('template-id' => $template_id, 'lasttime' => time());
             add_blog_option ($new_blog_id, 'add-cloned-sites', serialize($savearray));
             //get it back with:
@@ -159,67 +163,75 @@ class wpGrade_Trial {
 //        }
 
         // count succesfull and failed sites
-        if(!$error) {
-            _e("Job done, sucesfully created <a href=\"http://$fulldomain\">$fulldomain</a> with site id: $new_blog_id<br>", 'wpgrade' );
+        if(empty($error)) {
+            $this->log_to_file("Job done, sucesfully created $fulldomain with site id: $new_blog_id", $username );
         } else {
             // count failed sites
             $failed[] = $siteurl;
             $error = NULL;
         }
-
-
     }
 
     public function create_user_for_trial($username,$email){
 
+        $return = array();
+
         if ( username_exists( $username ) ){
 
-            $user_id = username_exists( $username );
-
+//            $user_id = username_exists( $username );
+            $return['id'] = username_exists( $username );
+            $return['msg'] = 'This user already exists.';
         } elseif ( email_exists($email) ) {
 
-            $user_id = email_exists($email);
-
+//            $user_id = email_exists($email);
+            $return['id'] = email_exists($email);
+            $return['msg'] = 'This user already exists.';
         } else {
             $random_password = wp_generate_password( $length=12, $include_standard_special_chars=false );
             $user_data = array(
                 'user_pass' => 'admin', //$random_password,
                 'user_login' => $username,
                 'user_nicename' => $username,
-                'user_email' => $email
+                'user_email' => $email,
+                'role' => $this->role
             );
 
-            $user_id = wp_insert_user( $user_data );
+//            $user_id = wp_insert_user( $user_data );
+            $return['id'] = wp_insert_user( $user_data );
+
+            if ( !is_wp_error($return['id']) ) {
+
+                $to      = $email;
+                $subject = 'Welcome to our trial '.$username;
+
+                ob_start();?>
+                What teh fuck ?
+                <?php $message = ob_get_clean();
+                $headers = 'From: contact@pixelgrade.com' . "\r\n";
+                // To send HTML mail, the Content-type header must be set
+                $headers .= 'MIME-Version: 1.0' . "\r\n";
+                $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+                mail($to, $subject, $message, $headers);
+
+                $return['msg'] = 'User created. An email was sent to you with credentials.';
+            }
+
         }
 
-
-        return $user_id;
+        return $return;
     }
 
-}
+    public function clear_old_trials() {
 
-function you_need_access() {
-//    $file = '/usr/share/nginx/trial/test.txt';
-//
-//    // Open the file to get existing content
-//    $current = file_get_contents($file);
-//// Append a new person to the file
-//    $current .= "Cron with NO NO NO access \n";
-//// Write the contents back to the file
-//    file_put_contents($file, $current);
+        global $wpdb;
 
-}
+        // life sucks ... you need to exclude the blog demos from here
+        // 1 = the godfather
+        // 2 = senna
+        // 3 = swipe
 
-function clear_old_trials(){
-    // get a list with all blog id's
-    global $wpdb;
-
-    // life sucks ... you need to exclude the blog demos from here
-    // 1 = the godfather
-    // 2 = senna
-    // 3 = swipe
-
-    $blogs = $wpdb->get_results("
+        $blogs = $wpdb->get_results("
         SELECT blog_id
         FROM {$wpdb->blogs}
         WHERE site_id = '{$wpdb->siteid}'
@@ -231,43 +243,31 @@ function clear_old_trials(){
         AND blog_id != 3
     ");
 
-    ob_start();
+        ob_start();
 
-    foreach ( $blogs as $blog ) {
+        foreach ( $blogs as $blog ) {
 
-        // the freaking date
-        $registered_date = get_blog_details($blog->blog_id)->registered;
-        $now = new DateTime();
-//        echo '<pre>';
-//        var_dump( $registered_date );
-//        echo '</pre>';
+            // the freaking date
+            $registered_date = get_blog_details($blog->blog_id)->registered;
+            $now = new DateTime();
 
-        $start_date = new DateTime( $registered_date );
-        $since_start = $start_date->diff( $now );
-//        echo $since_start->days.' days total<br>';
-//        echo $since_start->y.' years<br>';
-//        echo $since_start->m.' months<br>';
-//        echo $since_start->d.' days<br>';
-//        echo $since_start->h.' hours<br>';
-//        echo $since_start->i.' minutes<br>';
-//        echo $since_start->s.' seconds<br>';
+            $start_date = new DateTime( $registered_date );
+            $since_start = $start_date->diff( $now );
 
-        if ( $since_start->h >= 3 ) {
-            echo '<p>I\'m kinda sorry but your are cut out</p>';
+            if ( $since_start->i >= 1 ) {
+                wpmu_delete_blog( $blog->blog_id , true);
 
-            wpmu_delete_blog( $blog->blog_id , true);
-
-        } else {
-
-            echo '<p>you will live to tell teh story</p>';
+                $this->log_to_file('Blog: '. $blog->blog_id .' expired.Deleting ...', "CRONTAB" );
+            } else {
+//              echo '<p>you will live to tell teh story</p>';
+            }
 
         }
 
+        die();
     }
 
-    echo ob_get_clean();
-    exit('Ciao!');
 }
 
-add_action('wp_ajax_clear_old_trials', 'clear_old_trials');
-add_action('wp_ajax_nopriv_clear_old_trials', 'you_need_access');
+global $trial; // ugly
+$trial = new wpGrade_Trial();
